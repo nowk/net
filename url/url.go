f025e26ec4993d0972cfb7721825817958164c09
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2016-03-25 15:33:16 -0400

    Initial commit

---
+++ url/url.go
@@ -0,0 +1,70 @@
+package url
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+type URL struct {
+	*url.URL
+
+	// Port is the :8080 without the :
+	Port string
+
+	// HostOnly is the host only, this will never include any port information
+	HostOnly string
+}
+
+var (
+	schemePrefix = regexp.MustCompile("([a-z]+:)?(//)")
+
+	// shortcut to url.Parse
+	parse = url.Parse
+)
+
+func Parse(str string) (*URL, error) {
+	// force anonymous porotocol to allow url.Parse to parse the string
+	// properly
+	if !schemePrefix.MatchString(str) {
+		str = "//" + str
+	}
+
+	// parse the url, through net/url
+	neturl, err := parse(str)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		host, port = parseHostPort(neturl.Host)
+
+		u = &URL{
+			URL: neturl,
+
+			Port:     port,
+			HostOnly: host,
+		}
+	)
+
+	return u, nil
+}
+
+// parseHostPort parses the host:port at :. This is similar to net.SplitHostPort
+// but does not error when there is no port found, port is just empty.
+func parseHostPort(str string) (string, string) {
+	var (
+		host string
+		port string
+
+		i = strings.Index(str, ":")
+	)
+	if i == -1 {
+		return str, ""
+	}
+
+	host = str[:i]
+	port = str[i+1:]
+
+	return host, port
+}
---
+++ url/url_test.go
@@ -0,0 +1,77 @@
+package url
+
+import (
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+func TestParseReturnsAUrlWithPort(t *testing.T) {
+	var cases = [][]string{
+		{"http://example.com:8080", "http"},
+		{"//example.com:8080", ""},
+		{"example.com:8080", ""},
+	}
+
+	for _, v := range cases {
+		var (
+			giv = v[0]
+
+			u, err = Parse(giv)
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var (
+			exp = &URL{
+				URL: &url.URL{
+					Scheme: v[1],
+					Host:   "example.com:8080",
+				},
+				Port:     "8080",
+				HostOnly: "example.com",
+			}
+
+			got = u
+		)
+		if !reflect.DeepEqual(exp, got) {
+			t.Errorf("expected %s, got %s", exp, got)
+		}
+	}
+}
+
+func TestParseReturnsAUrl(t *testing.T) {
+	var cases = [][]string{
+		{"http://example.com", "http"},
+		{"//example.com", ""},
+		{"example.com", ""},
+	}
+
+	for _, v := range cases {
+		var (
+			giv = v[0]
+
+			u, err = Parse(giv)
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var (
+			exp = &URL{
+				URL: &url.URL{
+					Scheme: v[1],
+					Host:   "example.com",
+				},
+				Port:     "",
+				HostOnly: "example.com",
+			}
+
+			got = u
+		)
+		if !reflect.DeepEqual(exp, got) {
+			t.Errorf("expected %s, got %s", exp, got)
+		}
+	}
+}
